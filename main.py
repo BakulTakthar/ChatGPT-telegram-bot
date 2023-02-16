073bdbdf14c41a92677a3b11ab40b63dc1b1a755
bakul takthar <bakul takthar>
2023-02-16 21:37:13 +0530

    second commit

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import openai
+from telegram.ext import *
 
 openai.api_key = "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"
 
 model_engine = "text-davinci-003"
 
-
+API_KEY_TELEGRAM = "6043159805:AAHCzg9_IKnoDhtOAAOJhi3Jmn2MMLY_3tc"
 
 start = input("to start press any button.. \n to suspend press x")
+
+def message_command(update, context):
+    text_from_user = str(update.message.text).lower()
+    response_from_bot = response
+
 while start != "x":
     prompt = input("you >>")
     
@@ -24,3 +30,12 @@ while start != "x":
 
     if prompt == "exit":
         break
+    
+def start_command(update, context):
+    update.message.reply_text("type something and get started with lord bakul's wonderful creation!")
+    
+def help_command(update, context):
+    update.message.reply_text("help yourself! atmnirbhar baniye!")
+    
+    
+
